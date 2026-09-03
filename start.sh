65e6d8544e2f74859ab2368c70f275c9c97c8c4f
Roberto Fausti <robertofausti@cnr.it>
2026-09-03 18:35:06 +0200

    add ROS_DOMAIN_ID

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Check the ROS domain id passed as first argument
+if [[ "$1" =~ ^[0-9]+$ ]]; then
+    export ROS_DOMAIN_ID=$1
+    echo "Using ROS_DOMAIN_ID=$ROS_DOMAIN_ID"
+else
+    echo "Error: first argument must be a number (ROS_DOMAIN_ID)."
+    echo "Usage: $0 <ROS_DOMAIN_ID>"
+    exit 1
+fi
+
+touch ros_domain_id
+echo "${ROS_DOMAIN_ID}" > ros_domain_id
+
 # Specify the container name
 CONTAINER_NAME="drims2"
 IMAGE_NAME="smentasti/drims2:2026"
@@ -29,4 +42,4 @@ else
     echo "Container $CONTAINER_NAME does not exist."
 fi
 
-docker run -it  --group-add dialout --user drims --privileged -v /dev:/dev -v /dev/bus/usb:/dev/bus/usb --device=/dev/bus/usb --device-cgroup-rule='c 189:* rmw'  -v /etc/udev/rules.d:/etc/udev/rules.d --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --net=host --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="$(pwd)/drims_ws:/home/drims/drims_ws" --volume="$(pwd)/bags:/home/drims/bags"  --name drims2 -w /home/drims $IMAGE_NAME
+docker run -it  --group-add dialout --user drims --privileged --env="ROS_DOMAIN_ID" -v /dev:/dev -v /dev/bus/usb:/dev/bus/usb --device=/dev/bus/usb --device-cgroup-rule='c 189:* rmw'  -v /etc/udev/rules.d:/etc/udev/rules.d --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --net=host --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="$(pwd)/drims_ws:/home/drims/drims_ws" --volume="$(pwd)/bags:/home/drims/bags"  --name drims2 -w /home/drims $IMAGE_NAME
